e58f46967ecbdbdd42ec943adb7c9272aab3fbae
Andres Sanchez <El_AndreSs99@hotmail.com>
2020-03-12 12:50:06 -0700

    Avanzando en teclas

--- PiaNO/MainWindow.xaml.cs
+++ PiaNO/MainWindow.xaml.cs
@@ -13,6 +13,9 @@ using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
 
+using NAudio.Wave;
+using NAudio.Wave.SampleProviders;
+
 namespace PiaNO
 {
     /// <summary>
@@ -20,11 +23,38 @@ namespace PiaNO
     /// </summary>
     public partial class MainWindow : Window
     {
+        private WaveOut waveOut;
+        private MixingSampleProvider mixer;
         public MainWindow()
         {
             InitializeComponent();
+
+            waveOut = new WaveOut();
+
+            mixer = new MixingSampleProvider( WaveFormat.CreateIeeeFloatWaveFormat(44100,1));
+
+            mixer.ReadFully = true;
+
+            waveOut.Init(mixer);
+
+            waveOut.Play();
+
+
         }
 
- 
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+          
+            var nota_do =
+                new SignalGenerator(44100,1)
+                {
+                    Gain = 5,
+                    Frequency = 261.6,
+                    Type = SignalGeneratorType.Sin,
+
+                }.Take(TimeSpan.FromMilliseconds(250));
+
+            mixer.AddMixerInput(nota_do);
+        }
     }
 }
